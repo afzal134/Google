ce72217e7454e10b063d05a16272685a59b660ce
91739 <91739@LAPTOP-8DFG2JS8>
2021-08-10 21:13:57 +0530

    Added to Sub branch

---
+++ src/test/java/full/Google/Apple.java
@@ -0,0 +1,14 @@
+package full.Google;
+
+public class Apple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("afzal new");
+		
+		System.out.println("afzal");
+
+	}
+
+}
